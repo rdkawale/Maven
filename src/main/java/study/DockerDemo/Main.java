71f7d472efeeff11d756a33c1039c13b2360acb7
rushi <imrushi1163@gmail.com>
2022-01-24 20:29:11 +0530

    commiting

---
+++ src/main/java/study/DockerDemo/Main.java
@@ -0,0 +1,16 @@
+package study.DockerDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+
+	}
+
+	public static boolean checkIfInputIsAnEvenNumber(int number) {
+		
+		return number % 2 == 0;
+	}
+
+}
---
+++ src/test/java/study/DockerDemo/TestMain.java
@@ -0,0 +1,15 @@
+package study.DockerDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void testInputIsEven()
+	{
+		assertTrue(Main.checkIfInputIsAnEvenNumber(22));  //assertion
+	}
+
+}
